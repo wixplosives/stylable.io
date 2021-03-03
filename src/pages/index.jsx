2bd23f59d79cbb96cd97ac7e40fe680432c37416
Tom Raviv <tomr@wix.com>
2021-03-03 17:30:15 +0200

    more styling fixes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -61,8 +61,8 @@ function Home() {
   const { siteConfig = {} } = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={siteConfig.title}
+      description="A modern CSS pre-processor built for components"
     >
       <header className={clsx("hero hero--primary", styles.heroBanner)}>
         <div className="container">
