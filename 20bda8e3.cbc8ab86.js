f2c1cecd91d56c4d415ca58cc8bf59a64231743c
Tom Raviv <tomr@wix.com>
2021-03-07 18:37:09 +0200

    Deploy website - based on 6e62765471a4347b0c4feea4cac93a4b04898b71

---
+++ 20bda8e3.cbc8ab86.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{80:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Hola","permalink":"/stylable.io/blog/hola"}]}')}}]);
