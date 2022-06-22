93babeaa316e36db02ccb11435c7333bfe4e0f9b
Tom Raviv <tomr@wix.com>
2022-06-22 20:46:53 +0300

    chore(deps): upgrade to latest stable - add pleb config for pinned versions

---
+++ pleb.config.mjs
@@ -0,0 +1,7 @@
+export default {
+  pinnedPackages: [
+    { name: "react", reason: "v18 not yet supported by docusaurus" },
+    { name: "react-dom", reason: "v18 not yet supported by docusaurus" },
+    { name: "@types/react", reason: "v18 not yet supported by docusaurus" },
+  ],
+};
