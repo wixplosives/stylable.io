b964b5094847dec3573ce54308017d9cefd563a8
Barak Igal <baraki@wix.com>
2022-07-31 13:22:22 +0300

    playground working

--- src/pages/playground.jsx
+++ src/pages/playground.jsx
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Layout from "@theme/Layout";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import styles from "./styles.module.css";
+import { useState } from "react";
+
+function getLocationSearch() {
+  return typeof window !== "undefined" ? window.location.search : "";
+}
 
 function Playground() {
   const context = useDocusaurusContext();
+  const [state, setState] = useState({});
   const { siteConfig = {} } = context;
+  const iframeRef = useRef(null);
+
+  useEffect(() => {
+    const handler = ({ data }) => {
+      console.log("!", { data });
+      if (data?.infoResponse?.location) {
+        const loc = JSON.parse(data.infoResponse.location);
+        history.replaceState({}, "", location.pathname + loc.search);
+      }
+    };
+    window.addEventListener("message", handler);
+    return () => {
+      window.removeEventListener("message", handler);
+    };
+  });
 
   return (
     <Layout
       title={siteConfig.title}
       description="A modern CSS pre-processor built for components"
       noFooter={true}
+      wrapperClassName={styles.playgroundWrapper}
     >
       <iframe
-        src="https://wixplosives.github.io/stylable-playground2/?filePath=%2Fsrc%2Fapp.st.css&type=code&stDebug=true"
+        ref={iframeRef}
+        src={
+          "https://wixplosives.github.io/stylable-playground2/" +
+          getLocationSearch()
+        }
         frameBorder={0}
-        style={{ height: "100%", width: "100%" }}
       />
     </Layout>
   );
