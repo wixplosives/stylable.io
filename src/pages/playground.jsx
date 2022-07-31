1a97d82c4cc731b82ce4c922b10b724692f0ff3d
Barak Igal <baraki@wix.com>
2022-07-31 13:26:41 +0300

    cleanups

--- src/pages/playground.jsx
+++ src/pages/playground.jsx
@@ -2,35 +2,14 @@ import React, { useEffect, useRef } from "react";
 import Layout from "@theme/Layout";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./styles.module.css";
-import { useState } from "react";
 
-function getLocationSearch() {
-  return typeof window !== "undefined" ? window.location.search : "";
-}
-
-function Playground() {
+export default function Playground() {
+  useSyncUrlToPlaygroundFrame();
   const context = useDocusaurusContext();
-  const [state, setState] = useState({});
-  const { siteConfig = {} } = context;
-  const iframeRef = useRef(null);
-
-  useEffect(() => {
-    const handler = ({ data }) => {
-      console.log("!", { data });
-      if (data?.infoResponse?.location) {
-        const loc = JSON.parse(data.infoResponse.location);
-        history.replaceState({}, "", location.pathname + loc.search);
-      }
-    };
-    window.addEventListener("message", handler);
-    return () => {
-      window.removeEventListener("message", handler);
-    };
-  });
 
   return (
     <Layout
-      title={siteConfig.title}
+      title={context.siteConfig.title}
       description="A modern CSS pre-processor built for components"
       noFooter={true}
       wrapperClassName={styles.playgroundWrapper}
@@ -47,4 +26,21 @@ function Playground() {
   );
 }
 
-export default Playground;
+function getLocationSearch() {
+  return typeof window !== "undefined" ? window.location.search : "";
+}
+
+function useSyncUrlToPlaygroundFrame() {
+  useEffect(() => {
+    const handler = ({ data }) => {
+      if (data?.infoResponse?.location) {
+        const loc = JSON.parse(data.infoResponse.location);
+        history.replaceState({}, "", location.pathname + loc.search);
+      }
+    };
+    window.addEventListener("message", handler);
+    return () => {
+      window.removeEventListener("message", handler);
+    };
+  });
+}
