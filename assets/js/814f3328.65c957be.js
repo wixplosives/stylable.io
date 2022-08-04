11507935bddedb363244031e6d8bcbaf99c3968c
tomrav <tomrav@users.noreply.github.com>
2022-08-04 17:33:50 +0000

    deploy: 2501b03732493dbf449a4ee900ca3b3b24d8afda

--- assets/js/814f3328.65c957be.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkstylable_io=self.webpackChunkstylable_io||[]).push([[2535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome to Stylable v5","permalink":"/blog/stylable-v5"}]}')}}]);
