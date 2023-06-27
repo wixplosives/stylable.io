593437490dac0c0d4bcf3153910afbad3f6778dd
Ido Rosenthal <idoros@gmail.com>
2023-06-27 16:26:51 +0300

    feat: support anchor root in button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,17 +3,36 @@ import { st, classes } from './button.st.css.js';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     className?: string;
-    onClick?: React.MouseEventHandler<HTMLButtonElement>;
+    type?: 'button';
+    text?: string;
+    icon?: (iconClassName: string) => React.ReactNode;
+    callToAction?: boolean;
+}
+interface ButtonAnchorProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+    className?: string;
+    type: 'a';
     text?: string;
     icon?: (iconClassName: string) => React.ReactNode;
     callToAction?: boolean;
 }
 
-export function Button({ className, onClick, text, icon, callToAction, ...attrs }: ButtonProps) {
-    return (
-        <button {...attrs} className={st(classes.root, { callToAction }, className)} onClick={onClick}>
-            {!icon ? <span className={classes.icon}></span> : icon(classes.icon)}
-            {text ? <span className={classes.text}>{text}</span> : null}
-        </button>
-    );
+export function Button(props: ButtonProps | ButtonAnchorProps) {
+    const { className, onClick, text, icon, callToAction, type, ...attrs } = props;
+    const iconChild = !icon ? <span className={classes.icon}></span> : icon(classes.icon);
+    const textChild = text ? <span className={classes.text}>{text}</span> : null;
+    if (props.type === 'a') {
+        return (
+            <a {...(attrs as ButtonAnchorProps)} className={st(classes.root, { callToAction }, className)}>
+                {iconChild}
+                {textChild}
+            </a>
+        );
+    } else {
+        return (
+            <button {...(attrs as ButtonProps)} className={st(classes.root, { callToAction }, className)}>
+                {iconChild}
+                {textChild}
+            </button>
+        );
+    }
 }
