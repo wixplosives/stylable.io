7b8e5b1f73099de22e45962bd539ef83f50be05c
Ido Rosenthal <idoros@gmail.com>
2023-07-04 11:26:19 +0300

    feat: add Decoration preserveAspectRatio prop
    - cleanup component

--- src/components/decoration.tsx
+++ src/components/decoration.tsx
@@ -1,23 +1,40 @@
 import React from 'react';
 import { st, classes } from './decoration.st.css';
 
-export function Decoration({ context, className }: { context: string[]; className?: string }) {
+type PreserveAspectRatio =
+    | 'none'
+    | 'xMinYMin'
+    | 'xMidYMin'
+    | 'xMaxYMin'
+    | 'xMinYMid'
+    | 'xMidYMid'
+    | 'xMaxYMid'
+    | 'xMinYMax'
+    | 'xMidYMax'
+    | 'xMaxYMax';
+
+export function Decoration({
+    context,
+    className,
+    preserveAspectRatio,
+}: {
+    context: string[];
+    className?: string;
+    preserveAspectRatio?: PreserveAspectRatio;
+}) {
     const contextClasses = context.map((name) => `dec--of-${name}`).join(' ');
     return (
         <svg
             className={st(classes.root, className, contextClasses)}
             xmlns="http://www.w3.org/2000/svg"
-            // width="10px"
-            // height="10px"
-            // style={{ position: 'absolute', left: '-50vw' }}
-            // preserveAspectRatio="none"
+            preserveAspectRatio={preserveAspectRatio}
             fill="none"
             viewBox="0 0 1000 1000"
         >
             <foreignObject x="0" y="0" width="1000" height="1000">
                 <div className={classes.htmlElement}></div>
             </foreignObject>
-            <polygon points="5,5 195,10 185,185 10,195" />
+            <path className={classes.path} />
         </svg>
     );
 }
