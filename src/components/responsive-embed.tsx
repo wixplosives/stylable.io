7f68253249207ddfd0221b4901d603cdda981e79
Avi Vahl <avi.vahl@wix.com>
2024-11-24 22:03:48 +0200

    security(deps): upgrade to latest stable (#77)
    - upgrade ci to use LTS node 22 and latest actions
    - ensure `npx tsc --noEmit`
    - regenerate lock file

--- src/components/responsive-embed.tsx
+++ src/components/responsive-embed.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { classes } from './responsive-embed.st.css';
 
-export function ResponsiveEmbed({ src }) {
+export function ResponsiveEmbed({ src }: { src: string }) {
     return (
         <div className={classes.root}>
             <iframe src={src} frameBorder="0" allowFullScreen></iframe>
